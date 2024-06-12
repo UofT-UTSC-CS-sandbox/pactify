6adfc3e7a2fb5bbc3da9cd71738c195e034d7dc5
Sohil Chanana <schanana2003@gmail.com>
2024-06-12 03:47:08 -0400

    Modified login with tailwind styling, added signin styling

--- pactify-app/src/Components/login.js
+++ pactify-app/src/Components/login.js
@@ -1,76 +1,48 @@
-function LoginPage(){
+import React from 'react';
+
+function LoginPage() {
     return (
-        <section1>
-            <h1 style={welcomeToStyle}>Welcome to</h1>
-            <h1 style={pactifyStyle}>Pactify</h1>
-            <div style={inputEmailStyle}>
-                <input type="text" id="email-id" autoFocus placeholder="Email"/>
+        <div className="min-h-screen flex flex-row justify-evenly place-content-start flex-wrap pt-40 bg-transparent">
+            <div>
+                <h1 className="text-4xl text-red-500 font-bold mb-2">Welcome to</h1>
+                <h1 className="text-9xl text-red-500 font-black mb-6">Pactify</h1>
             </div>
-            <div style={inputPasswordStyle}>
-                <input type="password" id="password-id" autoFocus placeholder="Password"/>
+            <div>
+                <div className="mb-4">
+                    <input 
+                        type="text" 
+                        id="email-id" 
+                        autoFocus 
+                        placeholder="Email" 
+                        className="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-4 focus:ring-red-500 w-auto min-w-full flex-grow"
+                    />
+                </div>
+                <div className="mb-4">
+                    <input 
+                        type="password" 
+                        id="password-id" 
+                        autoFocus 
+                        placeholder="Password" 
+                        className="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-4 focus:ring-red-500 w-auto min-w-full flex-grow"
+                    />
+                </div>
+                <div className="mb-4">
+                    <button 
+                        type="submit" 
+                        className="px-4 py-2 bg-red-500 text-white font-medium rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+                    >
+                        Login
+                    </button>
+                </div>
+                <p className="text-white mb-2">
+                    <a href="signUp" className="underline hover:text-gray-300">Don't have an account? Sign up!</a>
+                </p>
+                <p className="text-white">
+                    <a href="forgotPassword" className="underline hover:text-gray-300">Forgot your password?</a>
+                </p>
             </div>
-            <div style={inputButton} >
-                <button type="submit">login</button>
-            </div>
-            <p>
-                <a style={options} href="signUp">Don't have an account? Sign up!</a>
-                {/*NEED TO CREATE A SIGN UP PAGE AND ADD LINK for href--*/}
-            </p>
-            <p>
-                <a style={options} href="forgotPassword">Forgot your password?</a>
-                {/* NEED TO CREATE A FORGOT PASSWORD PAGE AND ADD LINK for href*/}
-            </p>
-        </section1>
-    )
+        </div>
+    );
 }
 
 export default LoginPage;
-
-const welcomeToStyle = {
-    color: '#eb5233',
-    position: 'absolute',
-    top: '15%',
-    left: '20%',
-    fontFamily:'Roboto, sans-serif',
-    fontWeight: 100,
-    fontSize: '50px',
-}
-
-const pactifyStyle = {
-    color: '#eb5233',
-    position: 'absolute',
-    top: '14%',
-    left: '20%',
-    fontFamily:'Gill Sans',
-    fontSize: '120px',
-}
-
-const inputEmailStyle = {
-    transform: 'scale(1.5)',
-
-    position: 'absolute',
-    top: '25%',
-    right: '25%',
-}
-
-const inputPasswordStyle = {
-    transform: 'scale(1.5)',
-
-    position: 'absolute',
-    top: '30%',
-    right: '25%',
-}
-
-const inputButton = {
-    position: 'absolute',
-    top: '30%',
-    right: '23%',
-}
-
-const options = {
-    color: 'white',
-    position: 'relative',
-    left: '63%',
-    top: '280px',
-    fontSize: '18px',
-}
