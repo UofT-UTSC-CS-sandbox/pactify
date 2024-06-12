070d46caaa59833d7ac2db279dd6e8be476ab557
Sohil Chanana <schanana2003@gmail.com>
2024-06-12 03:47:08 -0400

    Modified login with tailwind styling, added signin styling

--- pactify-app/src/App.js
+++ pactify-app/src/App.js
@@ -1,9 +1,11 @@
 import LoginPage from './Components/login.js';
+import SignupPage from './Components/signup.js';
 
 function App(){
     return(
-        <div style={{ background: 'linear-gradient(#160541f9, #ecddab)', backgroundSize: '350% 350%', height: '100vh', width: '100vw', overflow: 'hidden'}}>
+        <div className='bg-violet-950'>
             <LoginPage/>
+            {/* <SignupPage/> */}
         </div>
     )
 }
--- pactify-app/src/Components/signup.js
+++ pactify-app/src/Components/signup.js
@@ -1,7 +1,40 @@
-function SignUpPage(){
+import React from 'react';
+
+function SignupPage() {
     return (
-        <section1>
-            <h1>Sign Up!</h1>
-        </section1>
-    )
+        <div className="min-h-screen flex items-center justify-center bg-violet-950">
+            <div className="bg-amber-50 rounded-3xl shadow-lg p-8 w-full max-w-md">
+                <h2 className="text-3xl font-bold text-center mb-2">Create An Account</h2>
+                <p className="text-center mb-6">Enter your information in the fields below</p>
+                <div className="space-y-4">
+                    <input 
+                        type="text" 
+                        placeholder="Email" 
+                        className="block w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-4 focus:ring-red-500"
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="Password" 
+                        className="block w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-4 focus:ring-red-500"
+                    />
+                    <input 
+                        type="password" 
+                        placeholder="Re-enter Password" 
+                        className="block w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-4 focus:ring-red-500"
+                    />
+                </div>
+                <button 
+                    type="submit" 
+                    className="mt-6 w-full px-4 py-2 bg-red-500 text-white font-medium rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
+                >
+                    Create Account
+                </button>
+                <p className="text-center mt-4">
+                    <a href="login" className="text-black underline hover:text-gray-400">Already Have An Account? Sign In!</a>
+                </p>
+            </div>
+        </div>
+    );
 }
+
+export default SignupPage;
