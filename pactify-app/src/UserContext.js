4c725626a1e1272e8bb9c5ddae842b072c818fb8
Alfonso <alfonsopersonal@yahoo.com>
2024-06-16 20:27:50 -0400

    fixed null

--- pactify-app/src/UserContext.js
+++ pactify-app/src/UserContext.js
@@ -1,7 +1,11 @@
 // UserContext.js
 import React, { createContext, useState , useEffect} from 'react';
 import axios from 'axios';
+import Cookies from 'universal-cookie';
 
+
+
+const cookies = new Cookies();
 
 export const UserContext = createContext();
 axios.defaults.withCredentials = true;
@@ -9,14 +13,25 @@ export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   useEffect(() => {
     const fetchUserData = async () => {
+      const token = cookies.get('jwt');
+      if (!token) {
+        console.error('No token found');
+        return;
+      }
+
       try {
-        const response = await axios.get('http://localhost:5050/home'); 
+        const response = await axios.get('http://localhost:5050/home', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
         setUser(response.data.user);
         console.log(response.data);
       } catch (error) {
         console.error('Error fetching user data:', error);
       }
     };
+
     fetchUserData();
   }, []);
   return (
