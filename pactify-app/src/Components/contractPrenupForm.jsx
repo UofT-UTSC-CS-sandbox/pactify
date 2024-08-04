149373df812d5c77948c54729bb3113e51a5e2ed
Alfonso Acosta <alfonsocruzacosta2003@gmail.com>
2024-08-04 19:01:07 -0400

    lil debugging action

--- pactify-app/src/Components/contractPrenupForm.jsx
+++ pactify-app/src/Components/contractPrenupForm.jsx
@@ -690,8 +690,8 @@ function ContractPrenupForm() {
                             </label>
                             <RichEditor initialValue={response} onValueChange={setResponse} />
                             <button
-                                onClick={handleOpenSave}
-                                className=" mt-4 px-4 py-2 w-3/6 self-center bg-blue-500 text-white rounded-full hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 hover:scale-105"
+                                onClick={() => handleOpenSave(setIsSaveOpen)}
+                                className="px-4 py-2 bg-blue-500 text-white rounded"
                             >
                                 Save
                             </button>
