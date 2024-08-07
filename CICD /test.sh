345331d18b57f221ffb9b3d7c9f1ec87d141d226
Hammad Iqbal <hammadkhalidqamar@outlook.com>
2024-08-07 04:12:24 -0400

    Created the CI/CD folder

--- CICD /test.sh
+++ CICD /test.sh
@@ -3,10 +3,10 @@
 # Increase the wait time to ensure the service is up and running
 sleep 60
 
-# Check if the service is listening on the expected port
+# Check if the service is listening 
 netstat -tuln
 
-# Perform a simple curl test to check if the service is up
+# Check if the service is up
 curl -f http://localhost:5000 || {
   echo "Service is not running or not accessible"
   exit 1
